cee852c30c23703f59f4ddc37fde85c41d08e1dc
benNjogu <kamandemachafi@gmail.com>
2023-03-23 11:41:47 +0300

    checking an item in an array using includes.

---
+++ includes.js
@@ -0,0 +1,10 @@
+const myArray = [1, 2, 3, 4, 2];
+
+const value = myArray.includes(3);
+//console.log(value); //->true
+
+const value2 = myArray.includes(2, 3); //whether the array includes 2 from index 3
+console.log(value2);
+
+const newArray = [{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }];
+
